e081c48f80ed2a2b3c17418ab7678aa9fea33af2
consolelogkochan <ik.tenzan.096@gmail.com>
2025-06-27 10:40:27 +0700

    kadai_004アップロード
    kadai_004をアップロードします

---
+++ kadai_004/event.js
@@ -0,0 +1,8 @@
+// loadイベント処理
+$(window).on('load', function() {
+    console.log('loadイベントが発生しました');
+});
+// scrollイベント処理
+$(window).on('scroll', function() {
+    console.log('scrollイベントが発生しました');
+});
